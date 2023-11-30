767768cfdeee4f31aadb9575d16e090f41566ebc
Tamsin Williams <tamsinwilliams@xmos.com>
2023-11-30 17:47:45 +0000

    added conftest and pytest.ini to support uncollecting tests

---
+++ tests/conftest.py
@@ -0,0 +1,15 @@
+def pytest_collection_modifyitems(config, items):
+    selected = []
+    deselected = []
+
+    for item in items:
+        m = item.get_closest_marker("uncollect_if")
+        if m:
+            func = m.kwargs["func"]
+            if func(**item.callspec.params):
+                deselected.append(item)
+            else:
+                selected.append(item)
+
+    config.hook.pytest_deselected(items=deselected)
+    items[:] = selected
