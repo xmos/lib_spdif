d06415a1f6b765c618b63a86c20e775933624855
Daniel Pieczko <danielpieczko@xmos.com>
2024-02-29 14:13:47 +0000

    Cleanup capture.py and readback.py utils scripts

--- tests/utils/capture.py
+++ tests/utils/capture.py
@@ -1,22 +1,32 @@
 # Copyright 2014-2024 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
+import argparse
 from pathlib import Path
 
-# open input file (list of ints)
-f_in_name = "spdif.log"
-f_in_location = str(Path(__file__).parent / f_in_name)
-f_in = open(f_in_location, "r")
-lines = f_in.readlines()
-f_in.close()
+parser = argparse.ArgumentParser(
+    description="Reads an input file containing one integer per line representing an oversampled S/PDIF input stream and outputs a binary file representation of that input stream"
+)
+parser.add_argument(
+    "in_file",
+    help="Path to input file containing the data captured from the S/PDIF input port",
+)
+parser.add_argument(
+    "out_file",
+    help="Path to output file to create containing the binary representation of the data from the input file",
+)
+args = parser.parse_args()
 
-# make / open file to output binary to
-f_out_name = "comparison.stream"
-f_out_location = str(Path(__file__).parent / "streams" /f_out_name)
-f_out = open(f_out_location, "wb")
+in_file = Path(args.in_file)
+out_file = Path(args.out_file)
 
-# iterate through input lines writing to binary file
-for line in lines:
-    byte_array = int(line).to_bytes(4, 'little')
-    f_out.write(byte_array)
-f_out.close()
+if in_file.exists():
+    with open(in_file, "r") as f:
+        lines = f.readlines()
+
+    with open(out_file, "wb") as f:
+        for line in lines:
+            byte_array = int(line).to_bytes(4, "little")
+            f.write(byte_array)
+else:
+    print(f"Error: input file {args.in_file} does not exist")
--- tests/utils/readback.py
+++ tests/utils/readback.py
@@ -1,14 +1,22 @@
 # Copyright 2014-2024 XMOS LIMITED.
 # This Software is subject to the terms of the XMOS Public Licence: Version 1.
 
+import argparse
 from pathlib import Path
 
-f_out_name = "comparison.stream"
-f_out_location = str(Path(__file__).parent /".."/ "test_rx" / "streams" /f_out_name)
-print(f_out_location)
-f_out = open(f_out_location, "rb")
-content = "".join("{0:08b}".format(byte)[::-1] for byte in f_out.read())
-f_out.close()
+parser = argparse.ArgumentParser(
+    description="Loads binary data from a file containing an oversample S/PDIF stream, and prints it to standard output"
+)
+parser.add_argument(
+    "in_file", help="Path to file containing binary recorded S/PDIF stream"
+)
+args = parser.parse_args()
 
-print(content)
+in_file = Path(args.in_file)
 
+if in_file.exists():
+    with open(in_file, "rb") as f:
+        content = "".join("{0:08b}".format(byte)[::-1] for byte in f.read())
+    print(content)
+else:
+    print(f"Error: input file {args.in_file} does not exist")
