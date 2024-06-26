02961816403cc07e3093a88fe17584ceeef2f5e0
Ross Owen <ross@xmos.com>
2024-06-26 11:33:02 +0100

    - Apply -Wno-unusal-code to spdif_rx.xc - Apply -Wno-unusual-code to spdif_tx_reconfig_port()

--- lib_spdif/lib_build_info.cmake
+++ lib_spdif/lib_build_info.cmake
@@ -2,6 +2,8 @@ set(LIB_NAME lib_spdif)
 set(LIB_VERSION 6.1.0)
 set(LIB_INCLUDES api)
 set(LIB_COMPILER_FLAGS -O3)
+set(LIB_COMPILER_FLAGS_SpdifTransmitReconfigPort.xc -O3 -Wno-unusual-code)
+set(LIB_COMPILER_FLAGS_spdif_rx.xc -O3 -Wno-unusual-code)
 set(LIB_DEPENDENT_MODULES "")
 
 XMOS_REGISTER_MODULE()
