9b247ded0e0d3a1f89eccab7d3d27414999d1a5e
Ed <ed@xmos.com>
2024-09-13 14:37:28 +0100

    Examples use XCCM

---
+++ deps.cmake
@@ -0,0 +1,6 @@
+# Common dependencies for examples
+set(APP_DEPENDENT_MODULES   "lib_spdif"
+                            "lib_sw_pll(2.2.0)"
+                            "lib_xassert(4.2.0)"
+                            "lib_logging(3.2.0)"
+)
